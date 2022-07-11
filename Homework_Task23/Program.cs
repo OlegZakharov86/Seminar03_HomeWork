e9161ddf178c8565d1fe3cacdd9cc643ae3a3a42
OlegZakharov <varahas@mail.ru>
2022-07-11 22:08:24 +0300

    added homework task 23

---
+++ Homework_Task23/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 1; i <=a; i++)
+            {
+                PrintCube(i);
+            }
+        }
+        public static void PrintCube(int n)
+        {
+            Console.WriteLine(n * n * n);
+        }
+    }
+}
