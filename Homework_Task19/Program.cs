17fa9940642a3e11609eaa5918f04c7d25392ebe
OlegZakharov <varahas@mail.ru>
2022-07-11 21:13:50 +0300

    added homework task 19

---
+++ Homework_Task19/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите пятизначное число");
+            string a = Convert.ToInt32(Console.ReadLine()).ToString();
+            
+              if(a[0] == a[4] || a[1] == a[3])
+            {
+                Console.WriteLine($"Число {a} палиндром");
+            }
+            else
+            {
+                Console.WriteLine($"Число {a} не палиндром");
+            }  
+        
+            
+        }
+    }
+}
