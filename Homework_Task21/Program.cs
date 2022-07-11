7ce3240f6bc48117565bd2ab4f340f8a3ff1434a
OlegZakharov <varahas@mail.ru>
2022-07-11 22:01:56 +0300

    added homework task 21

---
+++ Homework_Task21/Program.cs
@@ -0,0 +1,35 @@
+﻿//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве
+
+//формула для нахождения расстояния между двумя точками в 3D пространстве: AB = √(xb - xa)2 + (yb - ya)2 + (zb - za)2 
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите координаты первой точки Xa, Ya, Za");
+            int Xa = Convert.ToInt32(Console.ReadLine());
+            int Ya = Convert.ToInt32(Console.ReadLine());
+            int Za = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Введите координаты второй точки Xb, Yb, Zb");
+            int Xb = Convert.ToInt32(Console.ReadLine()); 
+            int Yb = Convert.ToInt32(Console.ReadLine());
+            int Zb = Convert.ToInt32(Console.ReadLine());
+
+            int len = (Xb-Xa)*(Xb-Xa) + (Yb-Ya)*(Yb-Ya) + (Zb-Za)*(Zb-Za); 
+            double dist = Sqrt(len);
+            Console.WriteLine(dist);
+        }
+        public static double Sqrt (double d)
+        {
+            double n = Sqrt(d);
+            return n;
+        }
+        
+
+
+
+        
+    }
+}
